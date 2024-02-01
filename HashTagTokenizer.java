b4e1ea1142571bc82e417922a71c51816608a167
anan 975321 <anan.gara@post.runi.ac.il>
2024-02-01 12:43:58 +0200

    Check

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -3,27 +3,46 @@
 public class HashTagTokenizer {
 
 	public static void main(String[] args) {
-
+		
 		String hashTag = args[0];
 		String []dictionary = readDictionary("dictionary.txt");
 		breakHashTag(hashTag, dictionary);
+	
+
 	}
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
 
 		In in = new In(fileName);
+		int i = 0;
 
-		// Your code here
+		while (!in.isEmpty()){
+
+			dictionary[i] = in.readLine();
+			i++;
+
+		}
 
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+
+		for (int i = 0; i < dictionary.length; i++){
+
+			if (word.equals(dictionary[i])){
+
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
+
+		hashtag = hashtag.toLowerCase();
 
 		// Base case: do nothing (return) if hashtag is an empty string.
         if (hashtag.isEmpty()) {
@@ -34,7 +53,21 @@ public class HashTagTokenizer {
 
         for (int i = 1; i <= N; i++) {
 		
+			if(existInDictionary(hashtag.substring(0, i), dictionary)){
+
+				System.out.println(hashtag.substring(0, i));
+				breakHashTag(hashtag.substring(i, N), dictionary);
+
+			}
+
+			
+			
+
+
+			}
+
+
         }
     }
 
-}
+
